31ce8407ba91ccf60f5e6cd597d4154f53cd17b7
Andrew Wilson <andrew@lightsinthesky.io>
2021-02-13 09:59:50 -0400

    initial commit

---
+++ src/components/Box.js
@@ -0,0 +1,5 @@
+import React, { useRef, useState } from 'react';
+
+export default function Box(props) {
+    return <div {...props}>Box Component</div>;
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Box from "./components/Box"
+import './styles.css';
+
+ReactDOM.render(
+    <div>
+        <Box />
+    </div>,
+    document.getElementById('root')
+);
